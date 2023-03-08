ccb665ffdfaaad5a6cdfa087f52c4211b3679860
grantcary <imgrantcary@gmail.com>
2023-03-08 00:57:35 -0800

    object structure reconfigured

--- src/primatives.py
+++ src/primatives.py
@@ -1,65 +1,56 @@
+class Tools:
+  def slope(self, origin: list[float], target: list[float]):
+    return [target[i] - origin[i] for i in range(len(origin))]
+
 class Point():
   def __init__(self, x: float, y: float, z: float):
-    self.x = x
-    self.y = y
-    self.z = z
+    self.xyz: list = [x, y, z]
     
   def translate(self, x: float, y: float, z: float) -> None:
-    self.x += x
-    self.y += y
-    self.z += z
+    self.xyz = [pos + [x, y, z][i] for i, pos in enumerate(self.xyz)]
 
   def __repr__(self):
-    return f"Point({self.x}, {self.y}, {self.z})"
+    return f"Point({self.xyz[0]}, {self.xyz[1]}, {self.xyz[2]})"
 
 class Vertex():
-  def __init__(self, x: float, y: float, z: float):
-    self.x = x
-    self.y = y
-    self.z = z
+  def __init__(self, x: float, y: float, z: float, faces: list['Face'] = None) -> None:
+    self.xyz: list = [x, y, z]
+    self.faces: list = faces
 
   def translate(self, x: float, y: float, z: float) -> None:
-    self.x += x
-    self.y += y
-    self.z += z
-    
+    self.xyz = [pos + [x, y, z][i] for i, pos in enumerate(self.xyz)]
+
   def moveto(self, target: Point) -> None:
-    self.x = target.x
-    self.y = target.y
-    self.z = target.z
+    self.xyz = [pos + [target.xyz][i] for i, pos in enumerate(self.xyz)]
 
   def __repr__(self):
-    return f"Vertex({self.x}, {self.y}, {self.z})"
+    return f"Vertex({self.xyz[0]}, {self.xyz[1]}, {self.xyz[2]})"
 
-# linked list point cloud
-class Edge():
-  def __init__(self, vertex: Vertex, connected: list['Edge'] = None):
-    self.vertex = vertex
-    self.connected = connected
+class Face(Tools):
+  def __init__(self, *vertices: list[Vertex]) -> None:
+    self.vertices = vertices
+    self.normal = self.normal()
 
-class Face():
-  def __init__(self, edges = []):
-    self.edges = edges
-
-  # TODO: create vertices from edges
   # TODO: calculate normal vector
+  def normal(self):
+    A = self.slope(self.vertices[1].xyz, self.vertices[0].xyz)
+    B = self.slope(self.vertices[2].xyz, self.vertices[0].xyz)
+    Nx = A[1] * B[2] - A[2] * B[1]
+    Ny = A[2] * B[0] - A[0] * B[2]
+    Nz = A[0] * B[1] - A[1] * B[0]
+    return [Nx, Ny, Nz]
 
-  # face/edge relation rules:
-  # you can't have an face on an open edge loop
-  # you can't have a face with less than 3 edges
-  # you can have an edge loop with no face
-
-class Object():
-  def __init__(self, vertices: list[Vertex] = None, edges: Edge = None):
-    self.vertices: list[Vertex] = vertices
-    self.edges: Edge = edges
-    self.origin: Point = self.set_origin() if self.vertices else None
+class Object(Tools):
+  def __init__(self, vertices: list[Vertex] = None, faces: list[Face] = None) -> None:
+    self.vertices: list = vertices
+    self.faces: list = faces
+    self.origin: Point = self.set_origin()
 
   def set_origin(self) -> Point:
-    points = [p for p in self.vertices]
-    x = (max(points, key=lambda p: p.x).x + min(points, key=lambda p: p.x).x) / 2
-    y = (max(points, key=lambda p: p.y).y + min(points, key=lambda p: p.y).y) / 2
-    z = (max(points, key=lambda p: p.z).z + min(points, key=lambda p: p.z).z) / 2
+    points = [p.xyz for p in self.vertices]
+    x = (max(points, key=lambda p: p[0])[0] + min(points, key=lambda p: p[0])[0]) / 2
+    y = (max(points, key=lambda p: p[1])[1] + min(points, key=lambda p: p[1])[1]) / 2
+    z = (max(points, key=lambda p: p[2])[2] + min(points, key=lambda p: p[2])[2]) / 2
     return Point(x, y, z)
 
   def translate(self, x: float, y: float, z: float) -> None:
@@ -68,23 +59,7 @@ class Object():
     self.origin = self.origin.translate(x, y, z)
 
   def moveto(self, target: Point) -> None:
-    x_delta = target.x - self.origin.x
-    y_delta = target.y - self.origin.y
-    z_delta = target.z - self.origin.z
+    x, y, z = self.slope(self.origin.xyz, target.xyz)
     for vertex in self.vertices:
-      vertex.translate(x_delta, y_delta, z_delta)
+      vertex.translate(x, y, z)
     self.origin = target
-
-  def traverse_edges(self) -> list[Point]:
-    def t(edge: Edge, found: list[Point]):
-      if edge.vertex in found:
-        return
-      found.append(edge.vertex)
-      if edge.connected == None:
-        return
-      for e in edge.connected:
-        t(e, found)
-        
-    found = []
-    t(self.edges, found)
-    return found
--- src/test.py
+++ src/test.py
@@ -1,90 +1,38 @@
-from primatives import Point, Vertex, Edge, Object
+from primatives import Point, Vertex, Face, Object
 from matplotlib import pyplot as plt
 import random
 import copy
 
-def randomized_point_cloud():
-  def create_object():
-    vertices = []
-    for _ in range(10):
-      vertices.append(Vertex(random.randrange(-10, 11), random.randrange(-10, 11), random.randrange(-10, 11)))
-    return Object(vertices)
-  obj = create_object()
-  p = obj.origin
-  print(f"Origin: {p.x}, {p.y}, {p.z}")
-
-  print("\nMoving object to random point\n")
-
-  obj.moveto(Point(random.randrange(-10, 11), random.randrange(-10, 11), random.randrange(-10, 11)))
-  p = obj.origin
-  for i in obj.vertices:
-    print(f"{i.x}, {i.y}, {i.z}")
-  print(f"Origin: {p.x}, {p.y}, {p.z}")
-
-def edge_loop():
-  tr = Point(2, 2, 0)
-  tl = Point(-2, 2, 0)
-  br = Point(2, -2, 0)
-  bl = Point(-2, -2, 0)
-
-  tr_e = Edge(tr)
-  tl_e = Edge(tl)
-  br_e = Edge(br)
-  bl_e = Edge(bl)
-
-  tr_e.vertices = tl_e
-  tl_e.vertices = bl_e
-  bl_e.vertices = br_e
-  br_e.vertices = tr_e
-
-  start = tr_e
-  for _ in range(8):
-    print(repr(start.point))
-    start = start.vertices
-
-def prism_mesh():
-  tf = Vertex(0, 2, 2)
-  blf = Vertex(-2, 2, 0)
-  brf = Vertex(2, 2, 0)
-  tb = Vertex(0, -2, 2)
-  blb = Vertex(-2, -2, 0)
-  brb = Vertex(2, -2, 0)
-  leg = Vertex(4, -2, 0)
-
-  tf_e = Edge(tf)
-  blf_e = Edge(blf)
-  brf_e = Edge(brf)
-  tb_e = Edge(tb)
-  blb_e = Edge(blb)
-  brb_e = Edge(brb)
-  leg_e = Edge(leg)
-
-  tf_e.connected = [tb_e, blf_e, brf_e]  
-  blf_e.connected = [brf_e, tf_e, blb_e]
-  brf_e.connected = [blf_e, tf_e, brb_e]
-  tb_e.connected = [tf_e, blb_e, brb_e]
-  blb_e.connected = [brb_e, tb_e, blf_e]
-  brb_e.connected = [blb_e, tb_e, brf_e, leg_e]
-
-  def random_traversal():
-    start = tf_e
-    for _ in range(12):
-      print(start.vertex)
-      rand = random.randint(0, 2)
-      start = start.connected[rand]
-
-  vertices = [tf, blf, brf, tb, blb, brb, leg]
-  return Object(vertices, tf_e).traverse_edges()
-
 def complex_edge_mesh():
   pass
 
-def plot_points_3D(points: list[Point, Vertex]):
+def square_pyramid_generator(width: float, height: float):
+  v0 = Vertex(0, 0, height)
+  v1 = Vertex(width, width, 0)
+  v2 = Vertex(-width, width, 0)
+  v3 = Vertex(-width, -width, 0)
+  v4 = Vertex(width, -width, 0)
+
+  f0 = Face(v0, v1, v2)
+  f1 = Face(v0, v2, v3)
+  f2 = Face(v0, v3, v4)
+  f3 = Face(v0, v4, v1)
+  f4 = Face(v1, v2, v3, v4)
+
+  v0.faces = [f0, f1, f2, f3]
+  v1.faces = [f0, f3, f4]
+  v2.faces = [f0, f1, f4]
+  v3.faces = [f1, f2, f4]
+  v4.faces = [f2, f3, f4]
+
+  return Object([v0, v1, v2, v3, v4], [f0, f1, f2, f3, f4])
+
+def plot_points_3D(points: list):
   fig = plt.figure()
   ax = fig.add_subplot(projection='3d')
 
   for i in points:
-    ax.scatter(i.x, i.y, i.z, marker='o')
+    ax.scatter(i[0], i[1], i[2], marker='o')
 
   ax.set_xlabel('X')
   ax.set_ylabel('Y')
@@ -93,6 +41,7 @@ def plot_points_3D(points: list[Point, Vertex]):
   plt.show()
 
 if __name__ == "__main__":
-  found = prism_mesh()
-  print(found)
-  plot_points_3D(found)
+  obj = square_pyramid_generator(4, 4)
+  obj.moveto(Point(0, 0, 0))
+  v = [i.xyz for i in obj.vertices]
+  plot_points_3D(v)
