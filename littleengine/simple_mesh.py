0070a7dd5c76347c387cc9daaf4486f7e935cd0e
grantcary <imgrantcary@gmail.com>
2023-03-17 03:56:57 -0700

    more functionality added to simple mesh object

--- littleengine/simple_mesh.py
+++ littleengine/simple_mesh.py
@@ -5,27 +5,46 @@ import numpy as np
 OBJ_FILE = '../test_objects/default_cube.obj'
 
 class Object():
-  def __init__(self, name = 'none', vertices = np.array([]), faces = np.array([])):
+  def __init__(self, name = 'none', vertices = np.array([]), faces = None, origin = None):
       self.name = name
-      self.vertices = vertices
-      self.f_to_v = {i: list(verts) for i, verts in enumerate(faces)} # maps what vertices a given face uses
+      self.vertices = vertices # list of raw vertices
+      if faces:
+        self.f_to_v = {i: list(verts) for i, verts in enumerate(faces)} # maps what vertices a given face uses
+        d = {}
+        for f, v in self.f_to_v.items():
+          for i in v:
+            if i not in d:
+              d[i] = []
+            d[i].append(f)
+        self.v_to_f = dict(sorted(d.items())) # maps what faces use a given vertex
+      self.origin = self.set_origin() if origin is None else origin # center of object based on max and min vertices on each axis
 
-      d = {}
-      for f, v in self.f_to_v.items():
-        for i in v:
-          if i not in d:
-            d[i] = []
-          d[i].append(f)
-      self.v_to_f = dict(sorted(d.items())) # maps what faces use a given vertex
+  # takes max and min of each axis and averages them to find the center
+  def set_origin(self):
+    x = (max(self.vertices, key=lambda p: p[0])[0] + min(self.vertices, key=lambda p: p[0])[0]) / 2
+    y = (max(self.vertices, key=lambda p: p[1])[1] + min(self.vertices, key=lambda p: p[1])[1]) / 2
+    z = (max(self.vertices, key=lambda p: p[2])[2] + min(self.vertices, key=lambda p: p[2])[2]) / 2
+    return np.array([x, y, z])
 
   def translate(self, x, y, z):
     self.vertices += np.array([x, y, z])
+
+  # move to a specific point
+  def moveto(self, x, y, z):
+    target = np.array([x, y, z])
+    dx, dy, dz = target - self.origin
+    for v in self.vertices:
+      v += np.array([dx, dy, dz])
+    self.origin = target
 
   def vertex_faces(self, vertex):
     return np.array(self.v_to_f[vertex])
   
   def face_vertices(self, face):
     return np.array([self.vertices[v] for v in self.f_to_v[face]])
+  
+  def __repr__(self):
+    return f"Object(name: {self.name}, vertices: {len(self.vertices)}, faces: {len(self.f_to_v)})"
 
 
 def plot_points_3D(points: list):
@@ -45,7 +64,6 @@ def plot_points_3D(points: list):
 if __name__ == "__main__":
   vertices, faces = OBJ(OBJ_FILE).read()
   o = Object('test', np.array(vertices), np.array(faces))
-  # print(o.vertices)
-  # print(o.faces) 
+  print(repr(o))
 
-  plot_points_3D(o.vertices)
+  # plot_points_3D(o.vertices)
