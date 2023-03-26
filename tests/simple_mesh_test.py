59dd7d57746886a9e51ce5e9a89f345d90c654a8
grantcary <imgrantcary@gmail.com>
2023-03-26 00:47:08 -0700

    reconfigured obj file import/export class

---
+++ tests/simple_mesh_test.py
@@ -0,0 +1,21 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from littleengine.simple_mesh import Object as SimpleObject
+from littleengine.read_file import OBJ
+import numpy as np
+
+OBJ_FILE = '../test_objects/default_cube.obj'
+
+if __name__ == "__main__":
+  obj = OBJ()
+  vertices, faces = obj.read(OBJ_FILE)
+  o = SimpleObject(name = obj.name, vertices = np.array(vertices), faces = np.array(faces))
+  new_obj = OBJ()
+  new_obj.write('test.obj', o)
+
+  # print('face to vertex', o.face_map)
+  # print('vertex to face', o.vertex_map)
+
+  # plot_points_3D(o.vertices)
