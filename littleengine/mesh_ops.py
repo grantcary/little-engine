74c85482fecb41379a429b9428dc285ddcc9787a
grantcary <imgrantcary@gmail.com>
2023-04-15 02:30:25 -0700

    added some comments

--- littleengine/mesh_ops.py
+++ littleengine/mesh_ops.py
@@ -12,6 +12,10 @@ class MeshOps:
         self.normals = normal / np.linalg.norm(normal, axis=1)[:, np.newaxis]
 
     def triangulate(self):
+        # Videos explaining the algorithm:
+        # https://www.youtube.com/watch?v=QAdfkylpYwc
+        # https://www.youtube.com/watch?v=hTJFcHutls8
+
         def signed_area(polygon):
             area = 0
             for i in range(len(polygon)):
@@ -30,43 +34,36 @@ class MeshOps:
             triangles = []
             while len(indices) > 3:
                 for i in range(len(indices)):
-                    i_prev, i_next = (i - 1) % len(indices), (i + 1) % len(indices) # get previous and next indices
-                    v0, v1, v2 = polygon[indices[[i_prev, i, i_next]]]
-                    if np.cross(v1 - v0, v2 - v0) <= 0: # check if angle is obtuse
+                    i_prev, i_next = (i - 1) % len(indices), (i + 1) % len(indices)             # get previous and next indices
+                    v0, v1, v2 = polygon[indices[[i_prev, i, i_next]]]                          # get vertices of the current triangle
+                    if np.cross(v1 - v0, v2 - v0) <= 0:                                         # check if angle is obtuse
                         continue
 
                     is_ear = True
-                    for j in range(len(indices)):
-                        if j == i_prev or j == i or j == i_next:
-                            continue
-                        point = polygon[indices[j]]
-                        if is_point_inside_triangle(point, v0, v1, v2):
-                            is_ear = False
-                            break
+                    for j in range(len(indices)):                                               # find ear loop
+                        if j != i_prev or j != i or j != i_next:                                # check if vertex j is not any of the current triangle's vertices
+                            if is_point_inside_triangle(polygon[indices[j]], v0, v1, v2):       # check if vertex j is inside the current triangle
+                                is_ear = False
+                                break
 
                     if is_ear:
-                        triangles.append((indices[i_prev], indices[i], indices[i_next]))
-                        indices = np.delete(indices, i)
+                        triangles.append((indices[i_prev], indices[i], indices[i_next]))        # add current triangle to the list of triangles
+                        indices = np.delete(indices, i)                                         # remove current vertex index from the list of available vertices
                         break
-
+                    
                 if len(indices) == 3:
-                    triangles.append(tuple(indices))
-
+                    triangles.append(tuple(indices))                                            # add the last triangle to the list of triangles
+          
             return triangles
         
         triangles = []
         for i, f in enumerate(self.faces):
             if len(f) > 3:
-                v = self.vertices[f]
-                dominant_axis = np.argmax(np.abs(self.normals[i]))
-                V_2D = np.delete(v, dominant_axis, axis=1)
-                
-                area = signed_area(V_2D) # check if polygon is clockwise or counter-clockwise
-                V_2D = V_2D[::-1] if area < 0 else V_2D
-
-                triangles.extend([f[list(triangle)] for triangle in ear_clipping(V_2D)])
+                verts = np.delete(self.vertices[f], np.argmax(np.abs(self.normals[i])), axis=1) # take all vertices of a given face and remove its largest axis
+                verts = verts[::-1] if signed_area(verts) < 0 else verts                        # check if polygon is clockwise or counter-clockwise
+                triangles.extend(f[list(triangle)] for triangle in ear_clipping(verts))         # triangulate polygon and add to list of triangles
             else:
                 triangles.append(f)
 
         self.faces = np.array(triangles)
-        self.set_normals()
+        self.set_normals()                                                                      # update normals
