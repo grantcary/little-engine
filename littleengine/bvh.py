201ffe4602febc8b4d2f272551054d81b9ee337d
grantcary <imgrantcary@gmail.com>
2023-05-06 03:01:15 -0700

    simple bounding box intersection working

--- littleengine/bvh.py
+++ littleengine/bvh.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import njit
 
 import littleengine.mesh as mesh
 
@@ -10,13 +11,43 @@ class BVH():
         self.right : BVH = right
 
 class Bounding_Box():
-    def __init__(self, xyz_mm):
-        self.xyz_mm = xyz_mm
+    def __init__(self, bounds):
+        self.bounds = bounds
 
-    def intersect(self):
-        pass
+    def intersect(self, ray_origin, ray_direction):
+        invdir = 1 / ray_direction
+        flipped_axes = np.argwhere(invdir < 0).flatten()
+        self.bounds[flipped_axes] = self.bounds[flipped_axes, ::-1]
 
-def bounding_box_generator(vertices):
+        mm = (self.bounds - ray_origin.reshape(3, 1)) * invdir.reshape(3, 1)
+        tmin, tmax, tymin, tymax, tzmin, tzmax = mm.flatten()
+
+        if (tmin > tymax) or (tymin > tmax):
+            return None
+        
+        if tymin > tmin:
+            tmin = tymin
+        if tymax < tmax:
+            tmax = tymax
+
+        if (tmin > tzmax) or (tzmin > tmax):
+            return None
+        
+        if tzmin > tmin:
+            tmin = tzmin
+        if tzmax < tmax:
+            tmax = tzmax
+
+        t = tmin
+
+        if t < 0:
+            t = tmax
+            if t < 0:
+                return None
+        
+        return t
+
+def bounds_generator(vertices):
     return np.vstack([np.array([vertices.max(axis=0), vertices.min(axis=0)]).T])
     # dimensions = np.abs(xyz_mm[:, 1] - xyz_mm[:, 0])
     # center = (xyz_mm[:, 1] + xyz_mm[:, 0]) / 2
--- tests/bounds_test.py
+++ tests/bounds_test.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import numpy as np
+from PIL import Image
 
 import littleengine.object as object
 import littleengine.camera as camera
@@ -13,26 +14,34 @@ SUZIE = '../test_objects/suzie.obj'
 CUBE = '../test_objects/default_cube.obj'
 ICOSPHERE = '../test_objects/icosphere.obj'
 
-objects = []
 suzie = object.Object('Monkey', SUZIE)
 suzie.material_type = 'diffuse'
 suzie.color = np.array([255, 0, 0])
 # suzie.translate(-2, 0, 0)
-objects.append(suzie)
-
-# cube = object.Object('Cube', CUBE)
-# cube.material_type = 'diffuse'
-# cube.translate(2, 0, 0)
-# cube.color = np.array([0, 255, 0])
-# objects.append(cube)
 
 cam = camera.Camera(90, aspect_ratio=1)
-cam.position = np.array([0, 0, 10])
+cam.position = np.array([0, 0, 2])
 cam.rotation = np.array([0, 180, 0])
 
 w, h = 10, 10
 cam_rays = render.camera_rays(w, h, cam)
-ray = cam_rays[(w * h) / 2]
+# ray_direction = cam_rays[(w * h) // 2]
 
-xyz_mm = bvh.bounding_box_generator(suzie.vertices)
-bb = bvh.Bounding_Box(xyz_mm)
+bounds = bvh.bounds_generator(suzie.vertices)
+print(bounds)
+bb = bvh.Bounding_Box(bounds)
+
+for ray_directions in cam_rays:
+    t = bb.intersect(cam.position, ray_directions)
+    if t != None:
+        phit = cam.position + ray_directions * t
+        print(phit)
+
+# img = np.full((h, w), 0 , dtype=np.uint8)
+# for i, ray_direction in enumerate(cam_rays):
+#     w, h = i // 10, i % 10
+#     img[w, h] = 255 if bb.intersect(cam.position, ray_direction) != None else 0
+
+# rendered_image = Image.fromarray(img, 'L')
+
+# rendered_image.show()
