ef493f4899c4a789eeccab460cb1492afc0e9e7e
grantcary <imgrantcary@gmail.com>
2023-05-05 23:39:54 -0700

    reconfiguring Bounding Box class

--- littleengine/bvh.py
+++ littleengine/bvh.py
@@ -10,17 +10,16 @@ class BVH():
         self.right : BVH = right
 
 class Bounding_Box():
-    def __init__(self, height, width, length, center = None):
-        self.height : float = height
-        self.width  : float = width
-        self.lenght : float = length
-        self.center : np.array[float] = center
+    def __init__(self, xyz_mm):
+        self.xyz_mm = xyz_mm
+
+    def intersect(self):
+        pass
 
 def bounding_box_generator(vertices):
-    xyz_mm = np.vstack([np.array([vertices.max(axis=0), vertices.min(axis=0)]).T])
-    dimensions = np.abs(xyz_mm[:, 1] - xyz_mm[:, 0])
-    center = (xyz_mm[:, 1] + xyz_mm[:, 0]) / 2
-    return dimensions, center
+    return np.vstack([np.array([vertices.max(axis=0), vertices.min(axis=0)]).T])
+    # dimensions = np.abs(xyz_mm[:, 1] - xyz_mm[:, 0])
+    # center = (xyz_mm[:, 1] + xyz_mm[:, 0]) / 2
 
 def bounding_volume_hierarchy(objects):
     pass
--- tests/bounds_test.py
+++ tests/bounds_test.py
@@ -5,6 +5,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import numpy as np
 
 import littleengine.object as object
+import littleengine.camera as camera
+import littleengine.render as render
 import littleengine.bvh as bvh
 
 SUZIE = '../test_objects/suzie.obj'
@@ -15,14 +17,22 @@ objects = []
 suzie = object.Object('Monkey', SUZIE)
 suzie.material_type = 'diffuse'
 suzie.color = np.array([255, 0, 0])
-suzie.translate(-2, 0, 0)
+# suzie.translate(-2, 0, 0)
 objects.append(suzie)
 
-cube = object.Object('Cube', CUBE)
-cube.material_type = 'diffuse'
-cube.translate(2, 0, 0)
-cube.color = np.array([0, 255, 0])
-objects.append(cube)
+# cube = object.Object('Cube', CUBE)
+# cube.material_type = 'diffuse'
+# cube.translate(2, 0, 0)
+# cube.color = np.array([0, 255, 0])
+# objects.append(cube)
 
-d, c = bvh.bounding_box_generator(cube.vertices)
-bb = bvh.Bounding_Box(*d, c)
+cam = camera.Camera(90, aspect_ratio=1)
+cam.position = np.array([0, 0, 10])
+cam.rotation = np.array([0, 180, 0])
+
+w, h = 10, 10
+cam_rays = render.camera_rays(w, h, cam)
+ray = cam_rays[(w * h) / 2]
+
+xyz_mm = bvh.bounding_box_generator(suzie.vertices)
+bb = bvh.Bounding_Box(xyz_mm)
